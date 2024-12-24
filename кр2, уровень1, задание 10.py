7f0cf862f7f01fc556a4855b36a023e2220cd0f2
efimovdaniil <efimovmoddychat73@gmail.com>
2024-12-24 13:58:44 +0300

    Update кр2, уровень1, задание 10.py

--- кр2, уровень1, задание 10.py
+++ кр2, уровень1, задание 10.py
@@ -6,18 +6,12 @@ x3 = randint(1,6)
 print(x1,x2,x3)
 
 #В матрице А размером 5 х 7 поменять местами строку, содержащую максимальный элемент в 3-м столбце, с 4-й строкой.
+import random
 
 rows, cols = 5, 7
-A = []
-print("Введите значения для матрицы 5x7 (по строкам):")
-for i in range(rows):
-    row = list(map(int, input(f"Введите 7 целых чисел для строки {i + 1}, разделенных пробелом: ").split()))
-    while len(row) != cols:
-        print(f"Ошибка: необходимо ввести ровно {cols} чисел.")
-        row = list(map(int, input(f"Введите 7 целых чисел для строки {i + 1}, разделенных пробелом: ").split()))
-    A.append(row)
+A = [[random.randint(1, 100) for _ in range(cols)] for _ in range(rows)]
 
-print("\nИсходная матрица A:")
+print("Исходная матрица A:")
 for row in A:
     print(row)
 
@@ -28,9 +22,9 @@ for i in range(1, rows):
     if A[i][2] > max_value:
         max_value = A[i][2]
         max_index = i
+
 A[max_index], A[3] = A[3], A[max_index]
 
 print("\nМатрица A после замены:")
 for row in A:
     print(row)
-
