059f476915fe3688eca4815d866cb871488cba90
efimovdaniil <efimovmoddychat73@gmail.com>
2025-02-23 18:18:41 +0300

    Update лабораторная9.py

--- лаба9/лабораторная9.py
+++ лаба9/лабораторная9.py
@@ -6,7 +6,6 @@ print(task1)
 
 
 def read_results(file_path):
-    """Читает результаты прыжков из файла."""
     results = {}
     with open(file_path, 'r') as file:
         for line in file:
@@ -18,7 +17,6 @@ def read_results(file_path):
 
 
 def find_best_results(results):
-    """Находит лучший результат для каждого участника."""
     best_results = {}
     for name, attempts in results.items():
         best_results[name] = max(attempts)
@@ -26,7 +24,6 @@ def find_best_results(results):
 
 
 def write_final_protocol(best_results, output_file):
-    """Записывает итоговый протокол в файл, отсортировав участников по лучшему результату."""
     sorted_results = sorted(best_results.items(), key=lambda item: item[1], reverse=True)
 
     with open(output_file, 'w') as file:
@@ -39,14 +36,10 @@ def write_final_protocol(best_results, output_file):
 if __name__ == "__main__":
     input_file = 'long_jump_results.txt'
     output_file = 'final_protocol.txt'
-
-    # Читаем результаты из файла
     results = read_results(input_file)
 
-    # Находим лучшие результаты
     best_results = find_best_results(results)
 
-    # Записываем итоговый протокол в файл
     write_final_protocol(best_results, output_file)
 
     print(f"Итоговый протокол записан в файл '{output_file}'.")
